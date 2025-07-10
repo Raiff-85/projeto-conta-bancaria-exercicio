a124f505ddf226130cc286338efb89fd241b01a6
Raiff Nóbrega <emanoelraiff@gmail.com>
2025-07-10 03:33:17 -0300

    Criacao dos metodos salvarSaldoEmArquivo, carregarSaldoDeArquivo, pagarBoleto e consultarChequeEspecial. Adaptacao do switch no Main e chamada dos metodos.

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,10 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class ContaBancaria {
     private String titular;
     private String agencia;
     private String numeroConta;
     private double saldo;
-    private double valorSaque;
-    private double chequeEspecial;
+    private double chequeEspecial = 300.00;
 
     public ContaBancaria(String titular,
                                String agencia,
@@ -34,16 +38,8 @@ public class ContaBancaria {
         return saldo;
     }
 
-    public double getValorSaque() {
-        return valorSaque;
-    }
-
     public double getChequeEspecial() {
         return chequeEspecial;
-    }
-
-    public void consultarSaldo(){
-        System.out.println("Saldo atual: " + this.saldo);
     }
 
     public void depositar(double valorDeposito){
@@ -53,13 +49,58 @@ public class ContaBancaria {
     }
 
     public boolean sacar(double valorSaque){
-        if (valorSaque > 0){
-            valorSaque -= saldo;
+        if (valorSaque <= 0){
+            this.saldo -= valorSaque;
             return false;
         } else if(this.saldo + this.chequeEspecial >= saldo){
             this.saldo -= valorSaque;
             return true;
         } else{
+            return false;
+        }
+    }
+
+    public boolean pagarBoleto(double valorBoleto) {
+        if (this.saldo + this.chequeEspecial >= valorBoleto) {
+            this.saldo -= valorBoleto;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double consultarChequeEspecial() {
+        if (this.saldo >= 0) {
+            return this.chequeEspecial;
+        } else {
+            double chequeDisponivel = this.saldo + this.chequeEspecial;
+            return Math.max(0, chequeDisponivel);
+        }
+    }
+
+    public boolean salvarSaldoEmArquivo() {
+        try (PrintWriter writer = new PrintWriter("saldo.txt")) {
+            writer.println(this.saldo);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean carregarSaldoDeArquivo() {
+        File arquivo = new File("saldo.txt");
+        if(!arquivo.exists()){
+            return salvarSaldoEmArquivo();
+        }
+
+        try (Scanner fileScanner = new Scanner(arquivo)) {
+            if (fileScanner.hasNextDouble()) {
+                this.saldo = fileScanner.nextDouble();
+                return true;
+            } else{
+                return false;
+            }
+        } catch (IOException e) {
             return false;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,104 @@
-import java.util.Objects;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
-        ContaBancaria conta = new ContaBancaria("Raiff",
-                "0272-5", "39226-2",0.0,
-                100.00);
-        System.out.println("Bem-vindo(a) ao Banco Sigma");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe a agência: ");
-        String agencia = scanner.next();
-        System.out.println("Informe a conta: ");
-        String numeroConta = scanner.next();
-        scanner.nextLine();
+public static void main(String[] args) {
+    ContaBancaria conta = new ContaBancaria("Raiff",
+            "0272-5", "39226-2", 0.0,
+            300.00);
 
-        int opcao = 0;
-        boolean continuar = true;
-
-        do {
-            System.out.println("-------------Menu-------------");
-            System.out.println("1 - Consulta de saldo");
-            System.out.println("2 - Depósito");
-            System.out.println("3 - Saques");
-            System.out.println("4 - Pagamento de boletos/títulos");
-            System.out.println("5 - Cheque especial");
-
-            System.out.println("Escolha uma das opções: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine();
-
-            switch(opcao){
-                case 1:
-                    System.out.printf("Seu saldo atual é de %.2f.\n ", conta.getSaldo());
-                    break;
-                case 2:
-                    System.out.println("Digite o valor para depósito: ");
-                    double valorDeposito = scanner.nextDouble();
-                    scanner.nextLine();
-                    conta.depositar(valorDeposito);
-                    System.out.printf("Seu depósito foi de %.2f.\n", valorDeposito);
-                    break;
-                case 3:
-                    System.out.println("Digite o valor para saque: ");
-                    double valorSaque = scanner.nextDouble();
-                    scanner.nextLine();
-                    System.out.printf("O valor do saque é de %.2f reais.\n", valorSaque);
-                    break;
-                case 4:
-                    System.out.println("O pagamento do boleto/título foi realizado.");
-                    break;
-                case 5:
-                    System.out.println("Cheque especial");
-                    break;
-                case 0:
-                    continuar = false;
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-                    break;
-            }
-
-            if (opcao != 0){
-                System.out.println("Deseja realizar mais ações? (Sim ou não): ");
-                String resposta = scanner.nextLine();
-                if(!resposta.equals("sim") && !resposta.equals("s")){
-                    continuar = false;
-                }
-        ;    }
-        } while (continuar);
-
-        System.out.println("\nAté breve!");
-        scanner.close();
+    boolean carregamentoBemSucedido = conta.carregarSaldoDeArquivo();
+    if (!carregamentoBemSucedido) {
+        System.err.println("AVISO INTERNO: Não foi possível carregar" +
+                " o arquivo. O saldo inicial será usado.");
     }
 
+    System.out.println("Bem-vindo(a) ao Banco Sigma!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe a agência: ");
+    String agencia = scanner.next();
+    System.out.println("Informe a conta: ");
+    String numeroConta = scanner.next();
+    scanner.nextLine();
+
+    int opcao = 0;
+    boolean continuar = true;
+
+    do {
+        System.out.println("-------------Menu-------------");
+        System.out.println("1 - Consulta de saldo");
+        System.out.println("2 - Depósito");
+        System.out.println("3 - Saques");
+        System.out.println("4 - Pagamento de boletos/títulos");
+        System.out.println("5 - Cheque especial");
+
+        System.out.println("Escolha uma das opções: ");
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                System.out.printf("Seu saldo atual é de %.2f.\n", conta.getSaldo());
+                break;
+            case 2:
+                System.out.println("Digite o valor para depósito: ");
+                double valorDeposito = scanner.nextDouble();
+                scanner.nextLine();
+                conta.depositar(valorDeposito);
+                System.out.printf("Seu depósito foi de %.2f.\n", valorDeposito);
+                break;
+            case 3:
+                System.out.println("Digite o valor para saque: ");
+                double valorSaque = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.printf("O valor do saque é de %.2f reais.\n", valorSaque);
+                break;
+            case 4:
+                System.out.println("Digite o valor do boleto: \n" +
+                        "Caso o saldo seja inferior ao valor do boleto, " +
+                        "o pagamento não será efetuado.");
+                double valorBoleto = scanner.nextDouble();
+                scanner.nextLine();
+
+                if (conta.pagarBoleto(valorBoleto)) {
+                    System.out.printf("O valor de %.2f reais foi pago com sucesso.\n", valorBoleto);
+                } else {
+                    System.out.println("Saldo insuficiente. Não foi possível realizar o pagamento.");
+                }
+                break;
+            case 5:
+                System.out.println("--- Cheque Especial ---");
+                double valorChequeDisponivel = conta.consultarChequeEspecial();
+
+                if (valorChequeDisponivel > 0) {
+                    System.out.printf("Você possui R$ %.2f de cheque especial disponível " +
+                            "para uso (de um limite total " +
+                            "de R$ %.2f).\n", valorChequeDisponivel, conta.getChequeEspecial());
+                } else if (conta.getSaldo() < 0) {
+                        System.out.printf("Você utilizou R$ %.2f do seu " +
+                                "limite de cheque especial.\n", Math.abs(conta.getSaldo()));
+                }else {
+                    System.out.println("Você não possui cheque especial " +
+                            "disponível ou não tem limite disponível.");
+                }
+                break;
+            case 0:
+                continuar = false;
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+
+        if (opcao != 0) {
+            System.out.println("Deseja realizar mais ações? (Sim ou não): ");
+            String resposta = scanner.nextLine();
+            if (!resposta.equals("sim") && !resposta.equals("s")) {
+                continuar = false;
+            }
+            ;
+        }
+    } while (continuar);
+
+    System.out.println("\nAté breve!");
+    scanner.close();
+    conta.salvarSaldoEmArquivo();
 }
